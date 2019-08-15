e963cc194903c0028ffaf0ef468705e71baeffae
rhiannanberry <rhiannanb97@gmail.com>
2019-08-14 23:25:56 -0400

    old stuff

---
+++ Assets/Utilities/ImageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ImageExtensions
+{
+    public static void SetAlpha(this Image i, float a) {
+        Color temp = i.color;
+        temp.a = a;
+        i.color = temp;
+    }
+
+}
