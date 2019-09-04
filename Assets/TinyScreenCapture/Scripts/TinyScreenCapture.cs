3a3e473efc7dc714583cf3ede99662191f64f9cb
rhiannanberry <rhiannanb97@gmail.com>
2019-09-04 13:56:05 -0400

    Added screencapping tool for use in editor

---
+++ Assets/TinyScreenCapture/Scripts/TinyScreenCapture.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class TinyScreenCapture : MonoBehaviour
+{
+	#if UNITY_EDITOR
+
+	//konstructor
+    private static TinyScreenCapture Instance { get; set; }
+
+    [Header ("Prefix for saved filed.")]
+    public string fileName = "screenshoot";
+    private Texture2D texture;
+
+    //do not destroy object when changing or reloading scenes
+	void Awake(){
+		if (Instance){
+			Destroy (gameObject);
+		} else {
+			Instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+    //listen for keyboard button press
+	void Update(){
+        //you can change selected keyboard button for different favorite button
+		if (Input.GetKeyDown("s"))
+			StartCoroutine(TinyCapture());
+	}
+
+    //capture screen
+    IEnumerator TinyCapture() {
+		//wait for graphics to render
+		yield return new WaitForEndOfFrame();
+
+		// create a texture to pass to encoding
+		texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+		//put buffer into texture
+		texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		texture.Apply();
+
+		//split the process up--ReadPixels() and the GetPixels() call inside of the encoder are both pretty heavy
+		yield return 0;
+
+		byte[] bytes = texture.EncodeToPNG();
+        //sufix for filename
+		string timestamp = System.DateTime.Now.ToString ("dd_MM_yyyy_HH_mm_ss");
+
+		//save image
+        File.WriteAllBytes(Application.dataPath + "/../Assets/TinyScreenCapture/ScreenCapture/"+ fileName +"_" + timestamp + ".png", bytes);
+	}
+
+	#endif
+}
---
+++ Assets/TinyScreenCapture/readme.txt
@@ -0,0 +1,29 @@
+TinyScreenCapture - tiny, simple utility for screen capturing
+
+
+Do you need tiny and simple utility for capturing screenshoots in Unity Editor?
+Tiny ScreenCapture can help you.
+
+
+Features:
+* just drag and drop prefab to scene
+* run your project and start taking screenshots
+* intended for Unity Editor
+* very tiny
+* well comented code
+
+
+Files structure:
+
+1) Prefabs - prefab object
+2) ScreenCapture - folder for saving screenshots
+3) Scripts - code for this asset
+
+
+How to use:
+
+1) Import asset into your project
+2) Drag and drop TinyScreenCapture prefab on your scene (if you have more than one scene, we suggest to drop it on start scene)
+3) Run you project and start taking screenshots (default keyboard key for screenshots is "s")
+4) Screenshots are saved under ScreenCapture folder
+5) If you want different size of screenshoots just change editor resolution and run a project
