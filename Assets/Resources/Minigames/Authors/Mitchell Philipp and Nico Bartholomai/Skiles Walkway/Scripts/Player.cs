6475118f0ed646cc20c61bb54f385e6de462458c
Mitchell Philipp <mgpalpha1@gmail.com>
2019-09-16 23:55:15 -0400

    Added basic movement and side-scrolling to 'Skiles Walkway'

---
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MinigameBehaviour
+{
+
+    Rigidbody2D r;
+    bool running = false;
+    public float moveForce = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        r = GetComponent<Rigidbody2D>();
+    }
+
+    protected override void OnStateEnter() {
+        running = true;
+    }
+
+    protected override void OnStateExit() {
+        running = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running) {
+            Vector2 newForce = new Vector2(0,0);
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                newForce += Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                newForce += Vector2.right;
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                newForce += Vector2.down;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                newForce += Vector2.left;
+            }
+            newForce.Normalize();
+            newForce *= moveForce * Time.deltaTime;
+            r.AddForce(newForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/PlayerCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MinigameBehaviour
+{
+    bool running = false;
+    public GameObject player;
+    public GameObject leftBarrier;
+    public GameObject rightBarrier;
+    public float borderMargin = 10;
+    float cameraY;
+    float cameraZ;
+    
+    protected override void OnStateEnter() {
+        running = true;
+    }
+
+    protected override void OnStateExit() {
+        running = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        cameraY = transform.position.y;
+        cameraZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running) {
+            float playerX = player.transform.position.x;
+            if (playerX < leftBarrier.transform.position.x + borderMargin) {
+                transform.position = new Vector3(leftBarrier.transform.position.x + borderMargin, cameraY, cameraZ);
+            } else if (playerX > rightBarrier.transform.position.x - borderMargin) {
+                transform.position = new Vector3(rightBarrier.transform.position.x - borderMargin, cameraY, cameraZ);
+            } else {
+                transform.position = new Vector3(playerX, cameraY, cameraZ);
+            }
+        }
+    }
+}
