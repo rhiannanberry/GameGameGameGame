afa1337894de3631ad6dca5dc059c40c5a37ca50
Mitchell Philipp <mgpalpha1@gmail.com>
2019-09-17 00:52:45 -0400

    Added basic enemy waving hand at player

---
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Arm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MinigameBehaviour
+{
+    bool running = false;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    protected override void OnStateEnter() {
+        running = true;
+    }
+
+    protected override void OnStateExit() {
+        running = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject == player && running) {
+            PersistentDataManager.run.GameLost();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/HandoutEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandoutEnemy : MinigameBehaviour
+{
+    bool running = false;
+    
+    public float rotationRange = 90;
+    public float rotationSpeed = 5;
+    public int rotationDirection = 1;
+
+    float currentRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        currentRotation = 0;
+    }
+
+    protected override void OnStateEnter() {
+        running = true;
+    }
+
+    protected override void OnStateExit() {
+        running = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running) {
+            if (rotationDirection == 1) {
+                transform.rotation = Quaternion.Euler(Vector3.forward * rotationSpeed * Time.deltaTime + transform.rotation.eulerAngles);
+                currentRotation += rotationSpeed * Time.deltaTime;
+            } else {
+                transform.rotation = Quaternion.Euler(Vector3.forward * -rotationSpeed * Time.deltaTime + transform.rotation.eulerAngles);
+                
+                currentRotation -= rotationSpeed * Time.deltaTime;
+            }
+            if (currentRotation > rotationRange / 2) {
+                rotationDirection = -1;
+            } else if (currentRotation < -rotationRange / 2) {
+                rotationDirection = 1;
+            }
+        }
+    }
+}
