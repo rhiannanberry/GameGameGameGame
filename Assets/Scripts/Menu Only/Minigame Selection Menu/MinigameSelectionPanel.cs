52124c3ad9466a851838a238ad69878632836514
rhiannanberry <rhiannanb97@gmail.com>
2019-10-18 19:47:11 -0400

    reorganizing so yall will stop making fun of me

---
+++ Assets/Scripts/Menu Only/Minigame Selection Menu/MinigameSelectionPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class MinigameSelectionPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject _buttonPrefab = null;
+    [SerializeField] private TMP_Dropdown _authorDropdown = null;
+
+    private void Start() {
+        DestroyExampleButtons();
+        GenerateMinigameButtons();
+        _authorDropdown.AddOptions(PersistentDataManager.minigameMasterList.Authors().Keys.ToList());
+    }
+
+    private void DestroyExampleButtons() {
+        foreach(Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    private void GenerateMinigameButtons() {
+        bool first = true;
+        foreach(Minigame m in PersistentDataManager.minigameMasterList.minigames) {
+            GameObject go = Instantiate(_buttonPrefab, transform);
+            Button b = go.GetComponent<Button>();
+            TextMeshProUGUI t = go.GetComponentInChildren<TextMeshProUGUI>();
+
+            if (first || m.GamePlayed) {
+                t.text = m.Name;
+            } else {
+                t.text = "???";
+            }
+
+            b.onClick.AddListener(() => ExitingMainMenu.StartNewRun(m));
+
+            if (!m.GameWon && !first) {
+                b.interactable = false;
+            }
+            
+            first = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Shared/Objects/MinigameList.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+
+[System.Serializable]
+public class MinigameList 
+{
+    private static readonly System.Random rnd = new System.Random();
+    public List<Minigame> minigames;
+
+    // Creates an empty list
+    public MinigameList() {
+        minigames = new List<Minigame>();
+    }
+
+    public MinigameList(bool populate) : this() {
+        if (populate) {
+            MinigameScriptableObject[] soArray = Resources.LoadAll<MinigameScriptableObject>("Minigames");
+            foreach(MinigameScriptableObject m in soArray) {
+                minigames.Add(m.ToObject());
+            }
+        }
+    }
+
+    public MinigameList(List<MinigameScriptableObject> soList) : this() {
+        foreach(MinigameScriptableObject m in soList) {
+            minigames.Add(m.ToObject());
+        }
+    }
+
+    public MinigameList(List<Minigame> mList) {
+        minigames = mList;
+    }
+
+    public MinigameList(params Minigame[] minigamesArray) {
+        minigames = minigamesArray.ToList();
+    }
+
+    public void UpdateList(MinigameList otherList) {
+        foreach(Minigame other in otherList.minigames) {
+            minigames.First( item => item.Name == other.Name).UpdateDetails(other);
+        }
+    }
+
+    public MinigameList RandomReorder() {
+        List<Minigame> newList = minigames;
+        newList.Shuffle();
+        return new MinigameList(newList);
+    }
+
+    public MinigameList RandomReorder(Minigame first) {
+        List<Minigame> newList = minigames;
+        newList.Shuffle();
+        if (newList.Contains(first)) newList.Remove(first);
+        newList.Insert(0, first);
+        return new MinigameList(newList);
+    }
+
+    public Dictionary<string, List<Minigame>> Authors() {
+        Dictionary<string, List<Minigame>> authors = new Dictionary<string, List<Minigame>>();
+        foreach(Minigame m in minigames) {
+            List<Minigame> old = new List<Minigame>();
+            if (!authors.ContainsKey(m.Author)) {
+                authors.Add(m.Author, new List<Minigame>());
+            }
+            authors.TryGetValue(m.Author, out old);
+            old.Add(m);
+            authors[m.Author] = old;
+        }
+        return authors;
+    }
+
+    public int Count { get {return minigames.Count; }}
+    /*private void Start() {
+
+        if (minigames == null) {
+            Debug.Log("GENERATING");
+            GenerateNewMinigameList();
+        }
+
+        #if (UNITY_EDITOR)
+            debug_minigames = minigames.ToArray();
+        #endif
+    }*/
+
+    //TODO: Include check for saved minigame data
+}
