acbc3529e2cfe4f22027f5e14373eca20984c6eb
Nico Bartholomai <nfbartholomai@gmail.com>
2019-11-02 02:09:25 -0400

    SkilesWalkway: added player sprites and animation state machine

--- Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Player.cs
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Player.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MinigameBehaviour
 {
 
     Rigidbody2D r;
+    Animator a;
     static bool running = false;
     public float moveForce = 5;
+    public float walkingThreshold = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         base.Start();
         r = GetComponent<Rigidbody2D>();
+        a = GetComponent<Animator>();
     }
 
     protected override void OnStateEnter() {
@@ -44,6 +48,12 @@ public class Player : MinigameBehaviour
             newForce.Normalize();
             newForce *= moveForce * Time.deltaTime;
             r.AddForce(newForce, ForceMode2D.Impulse);
+            Vector2 velocity;
+            velocity = r.velocity;
+            int direction = (int) (Math.Atan(velocity.y / velocity.x) * ((float) (2 / Math.PI)));
+            bool walking = Math.Pow(Math.Pow(velocity.x, 2) + Math.Pow(velocity.y, 2), .5) > walkingThreshold;
+            a.SetInteger("direction", direction);
+            a.SetBool("walking", walking);
         }
     }
 }
