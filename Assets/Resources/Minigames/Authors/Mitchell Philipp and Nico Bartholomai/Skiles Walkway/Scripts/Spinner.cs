4aa7a98b2af061e362b31a7b29d9c013a2c27f18
Mitchell Philipp <mgpalpha1@gmail.com>
2019-11-06 21:26:19 -0500

    SkilesWalkway: added strafing and spinning throwers

---
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Spinner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MinigameBehaviour
+{
+    public float spinSpeed = 5;
+    static bool running = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();    
+    }
+
+    protected override void OnStateEnter() {
+        if (!running) running = true;
+    }
+
+    protected override void OnStateExit() {
+        if (running) running = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running) {
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * spinSpeed));
+        }
+    }
+}
