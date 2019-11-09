4122af7496a804e0fc9f155cb5dd398f2398dc32
rhiannanberry <rhiannanb97@gmail.com>
2019-11-09 16:50:59 -0500

    flavor text

---
+++ Assets/Scripts/Minigame Only/UI/FlavorText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FlavorText : MonoBehaviour
+{
+    private TextMeshProUGUI txt;
+
+    public string[] win;
+    public string[] lose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+        System.Random random = new System.Random();
+        if (PersistentDataManager.run.gameWon) {
+            txt.text = win[random.Next(win.Length)];
+        } else {
+            txt.text = lose[random.Next(lose.Length)];
+        }
+
+    }
+
+
+}
