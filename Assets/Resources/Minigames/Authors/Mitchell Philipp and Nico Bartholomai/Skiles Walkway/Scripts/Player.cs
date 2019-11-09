acf6262575f34a96f200b68c7852b14514a9f969
Mitchell Philipp <mgpalpha1@gmail.com>
2019-11-09 00:23:08 -0500

    SkilesWalkway: Added assets and animations

--- Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Player.cs
+++ Assets/Resources/Minigames/Authors/Mitchell Philipp and Nico Bartholomai/Skiles Walkway/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MinigameBehaviour
     Animator a;
     static bool running = false;
     public float moveForce = 5;
-    public float walkingThreshold = 1;
+    int direction = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -32,26 +32,37 @@ public class Player : MinigameBehaviour
     void Update()
     {
         if (running) {
+            bool walking = false;
             Vector2 newForce = new Vector2(0,0);
             if (Input.GetKey(KeyCode.UpArrow)) {
                 newForce += Vector2.up;
+                walking = true;
             }
             if (Input.GetKey(KeyCode.RightArrow)) {
                 newForce += Vector2.right;
+                walking = true;
             }
             if (Input.GetKey(KeyCode.DownArrow)) {
                 newForce += Vector2.down;
+                walking = true;
             }
             if (Input.GetKey(KeyCode.LeftArrow)) {
                 newForce += Vector2.left;
+                walking = true;
             }
             newForce.Normalize();
             newForce *= moveForce * Time.deltaTime;
             r.AddForce(newForce, ForceMode2D.Impulse);
-            Vector2 velocity;
-            velocity = r.velocity;
-            int direction = (int) (Math.Atan(velocity.y / velocity.x) * ((float) (2 / Math.PI)));
-            bool walking = Math.Pow(Math.Pow(velocity.x, 2) + Math.Pow(velocity.y, 2), .5) > walkingThreshold;
+            if (newForce.x > 0) {
+                direction = 0;
+            } else if (newForce.x < 0) {
+                direction = 2;
+            } else if (newForce.y > 0) {
+                direction = 1;
+            } else if (newForce.y < 0) {
+                direction = 3;
+            }
+            Debug.Log(direction);
             a.SetInteger("direction", direction);
             a.SetBool("walking", walking);
         }
