40b8c1005e5071aff2be54ae2f2e19c05042c4e8
Rhiannan Berry <rhiannanb97@gmail.com>
2019-11-15 20:53:02 -0500

    importing games

---
+++ Assets/Resources/Minigames/Authors/Vinnie Davies/RoaringWheels/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 myPos;
+    public Transform raceCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = raceCar.position + myPos;
+    }
+}
---
+++ Assets/Resources/Minigames/Authors/Vinnie Davies/RoaringWheels/Movement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool isTouching = false;
+    AudioClip microphoneInput;
+    bool microphoneInitialized;
+    public Rigidbody carRigidbody;
+    public float speed = 10;
+    public float speedFactor;
+    public int gravfactor = 3;
+    public Collider target;
+    public float noise;
+    private void Awake()
+    {
+        if (Microphone.devices.Length > 0)
+        {
+            microphoneInput = Microphone.Start(null, true, 999, 44100);
+            microphoneInitialized = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision target)
+    {
+        print(target.gameObject.name);
+        if(target.gameObject.tag.Equals("GameState") == true)
+        {
+            if (PersistentDataManager.run != null) PersistentDataManager.run.GameWon();
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        noise = LevelMax();
+        if (isTouching)
+        {
+            speed = Mathf.Lerp(speed, 0, Time.deltaTime);
+            speed += noise;
+            carRigidbody.AddForce(transform.right * speed * speedFactor, ForceMode.Acceleration);
+        }
+        
+    }
+
+    public void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(Physics.gravity * gravfactor, ForceMode.Acceleration);
+        //RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, 1.5f))
+        {
+            isTouching = true;
+        } else
+        {
+            isTouching = false;
+        }
+    }
+
+    float LevelMax()
+    {
+        int _sampleWindow = 128;
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1); // null means the first microphone
+        if (micPosition < 0) return 0;
+        microphoneInput.GetData(waveData, micPosition);
+        // Getting a peak on the last 128 samples
+        for (int i = 0; i < _sampleWindow; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+}
