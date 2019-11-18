114bb3e3bcaa75c78ea3578c84439f22f0e8f64b
John Britti <jakebritti@gmail.com>
2019-11-17 23:19:15 -0500

    added switchboard minigame

---
+++ Assets/Resources/Minigames/Authors/John Britti/SwitchBoard/Scripts/Grabber.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Grabber : MonoBehaviour
+{
+    WireLead grabbed;
+    public float grabLerp = 10;
+    public AudioClip plugIn;
+    public AudioClip unPLug;
+    private AudioSource[] audioSources = new AudioSource[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < audioSources.Length; i++) {
+            audioSources[i] = gameObject.AddComponent<AudioSource>();
+        }
+        audioSources[0].clip = plugIn;
+        audioSources[1].clip = unPLug;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire1") && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)){
+            var lead = hit.transform.GetComponent<WireLead>();
+            if (lead) {
+                if (grabbed) {
+                    grabbed.Grabbed = false;
+                    grabbed.GetComponent<Collider>().enabled = true;
+                    grabbed.hole = lead.hole;
+                    grabbed = null;
+                    audioSources[0].pitch = Random.Range(0.7f, 1.3f);
+                    audioSources[0].Play();
+                }
+                audioSources[1].pitch = Random.Range(0.7f, 1.3f);
+                audioSources[1].Play();
+                grabbed = lead;
+                grabbed.Grabbed = true;
+                grabbed.GetComponent<Collider>().enabled = false;
+                grabbed.hole = null;
+            }
+            var hole = hit.transform.GetComponent<InputHole>();
+            if (hole && grabbed) {
+                audioSources[0].pitch = Random.Range(0.7f, 1.3f);
+                audioSources[0].Play();
+                grabbed.Grabbed = false;
+                grabbed.GetComponent<Collider>().enabled = true;
+                grabbed.hole = hole;
+                grabbed = null;
+                if (SwitchBoardController.instance.wires.All(w => w.CorrectHoles())) {
+                    PersistentDataManager.RUN.GameWon();
+                }
+            }
+        }
+        if (grabbed) {
+            var mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.transform.position.z;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            grabbed.transform.position = Vector3.Lerp(
+                grabbed.transform.position,
+                new Vector3(mousePos.x, mousePos.y, grabbed.transform.position.z),
+                Time.deltaTime * grabLerp
+            );
+        }
+    }
+}
---
+++ Assets/Resources/Minigames/Authors/John Britti/SwitchBoard/Scripts/SwitchBoardController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SwitchBoardController : MonoBehaviour
+{
+    SwitchLight[] lights;
+    InputHole[] holes;
+    [HideInInspector] public Wire[] wires;
+    int[] sourceHoles = {-1, -1, -1, -1};
+    int[] targetHoles = {-1, -1, -1, -1};
+    
+    [ColorUsageAttribute(false,true)]
+    public Color[] colors = new Color[4];
+    public Material[] wireMaterials = new Material[4];
+
+    public float lightDelay = 0.1f;
+
+    public static SwitchBoardController instance;
+
+    private void Start()
+    {
+        instance = this;
+        lights = GetComponentsInChildren<SwitchLight>();
+        holes = GetComponentsInChildren<InputHole>();
+        wires = FindObjectsOfType<Wire>();
+
+        for (int i = 0; i < sourceHoles.Length; i++) {
+            int target = Random.Range(0, holes.Length);
+            while (sourceHoles.Any(elem => elem == target)) {
+                target = Random.Range(0, holes.Length);
+            }
+            sourceHoles[i] = target;
+            wires[i].leads[0].hole = holes[target];
+            wires[i].PositionSetDirect(0, holes[target].transform.position);
+            wires[i].line.material = wireMaterials[i];
+            // lights[target].SetLight(colors[i]);
+        }
+
+        for (int i = 0; i < targetHoles.Length; i++) {
+            int target = Random.Range(0, holes.Length);
+            while (targetHoles.Any(elem => elem == target) || sourceHoles.Any(elem => elem == target)) {
+                target = Random.Range(0, holes.Length);
+            }
+            targetHoles[i] = target;
+            wires[i].leads[1].hole = holes[target];
+            wires[i].PositionSetDirect(1, holes[target].transform.position);
+            // lights[target].SetLight(colors[i]);
+        }
+
+        sourceHoles = new[]{-1, -1, -1, -1};
+        targetHoles = new[]{-1, -1, -1, -1};
+
+        for (int i = 0; i < sourceHoles.Length; i++) {
+            int target = Random.Range(0, holes.Length);
+            while (sourceHoles.Any(elem => elem == target)) {
+                target = Random.Range(0, holes.Length);
+            }
+            sourceHoles[i] = target;
+            wires[i].correctHoles[0] = holes[target];
+            // lights[target].SetLight(colors[i]);
+        }
+
+        for (int i = 0; i < targetHoles.Length; i++) {
+            int target = Random.Range(0, holes.Length);
+            while (targetHoles.Any(elem => elem == target) || sourceHoles.Any(elem => elem == target)) {
+                target = Random.Range(0, holes.Length);
+            }
+            targetHoles[i] = target;
+            wires[i].correctHoles[1] = holes[target];
+            // lights[target].SetLight(colors[i]);
+        }
+
+        StartCoroutine(StartLights());
+    }
+
+    IEnumerator StartLights() {
+        for(int i = 0; i < targetHoles.Length; i++) {
+            lights[sourceHoles[i]].SetLight(colors[i]);
+            lights[targetHoles[i]].SetLight(colors[i]);
+            yield return new WaitForSeconds(lightDelay);
+        }
+    }
+}
