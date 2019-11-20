536c979673095f03ab12b77e4a3ffb1312eea76d
Rhiannan Berry <rhiannanb97@gmail.com>
2019-11-19 22:04:41 -0500

    finicking with ui

---
+++ Assets/Scripts/Shared/UI/Behaviours/ClickSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickSound : MonoBehaviour
+{
+    void Awake() {
+        GetComponent<Button>().onClick.AddListener(() => {SoundManager._PlaySound("click");});
+    }
+}
